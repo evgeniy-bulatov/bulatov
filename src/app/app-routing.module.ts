0ee0d56efd1fe19f6bd7fcad55e8242155552dd8
BSLAY <eggormallll@gmail.com>
2023-04-22 17:24:22 +0300

    компонент home, addnew

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'addnew', loadChildren: () => import('./pages/addnew/addnew.module').then(m => m.AddnewModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
