431fd12ea77841dae46a6b84687b3f238ed3a950
BSLAY <eggormallll@gmail.com>
2023-04-28 09:25:18 +0300

    модуль home1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   { path: 'addnew', loadChildren: () => import('./pages/addnew/addnew.module').then(m => m.AddnewModule) },
   { path: 'user', loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule) },
-  { path: 'create', loadChildren: () => import('./pages/create/create.module').then(m => m.CreateModule) }
+  { path: 'create', loadChildren: () => import('./pages/create/create.module').then(m => m.CreateModule) },
+  { path: 'home1', loadChildren: () => import('./pages/home1/home1.module').then(m => m.Home1Module) }
 ];
 
 @NgModule({
